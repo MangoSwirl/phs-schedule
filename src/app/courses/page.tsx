2338364f1166cc2fa4bda057b485117da095fae0
MangoSwirl <collin.camerono@gmail.com>
2024-08-02 23:56:45 -0700

    Start courses page

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+
+import { Command, CommandInput, CommandList } from "@/components/ui/command";
+import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { PopoverAnchor } from "@radix-ui/react-popover";
+import { CommandEmpty, CommandItem, Command as CommandPrimitive } from "cmdk";
+import { useEffect, useRef, useState } from "react";
+
 const instructionalPeriods = ["1", "2", "3", "4", "5", "6", "7"] as const;
 
 type InstructionalPeriod = (typeof instructionalPeriods)[number];
@@ -9,6 +22,43 @@ export default function CoursesPage() {
       <p className="text-neutral-700">
         Enter your courses to display them in your schedule.
       </p>
+      <CourseSelector period="1" />
     </main>
+  );
+}
+
+function CourseSelector(props: { period: InstructionalPeriod }) {
+  const [popoverOpen, setPopoverOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    if (searchQuery.length > 0) {
+      setPopoverOpen(true);
+    } else {
+      setPopoverOpen(false);
+    }
+  }, [searchQuery]);
+
+  return (
+    <Command>
+      <Popover open={popoverOpen} onOpenChange={setPopoverOpen} modal={false}>
+        <PopoverAnchor>
+          <CommandPrimitive.Input asChild>
+            <Input
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </CommandPrimitive.Input>
+        </PopoverAnchor>
+        <PopoverContent>
+          <CommandList>
+            <CommandEmpty>No courses found</CommandEmpty>
+            <CommandItem>Course 1</CommandItem>
+            <CommandItem>Course 2</CommandItem>
+            <CommandItem>Course 3</CommandItem>
+          </CommandList>
+        </PopoverContent>
+      </Popover>
+    </Command>
   );
 }
