c79f167002967f80f5bb4187b411eafb1acd353a
MangoSwirl <collin.camerono@gmail.com>
2024-08-12 22:52:45 -0700

    Fix caching with redirects based on the current date

--- src/app/day/page.tsx
+++ src/app/day/page.tsx
@@ -1,6 +1,8 @@
 import { DateTime } from "luxon";
 import { redirect } from "next/navigation";
 
+export const revalidate = 0;
+
 export default function Page() {
   // Redirect to today
   redirect(`/day/${DateTime.now().toFormat("yyyy-LL-dd")}`);
